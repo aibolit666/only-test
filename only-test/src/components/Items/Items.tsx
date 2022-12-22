e859b4a7cde673ce3bc50779dbb7318e06adede2
Pavel <pavel.priladyshev@gmail.com>
2022-12-22 20:43:29 +0300

    feat: add spin logic

--- only-test/src/components/Items/Items.tsx
+++ only-test/src/components/Items/Items.tsx
@@ -3,30 +3,42 @@ import gsap from 'gsap';
 import './style.scss';
 
 const Items = () => {
-  const rotateItem = () => {
+  let reps: number;
+  const rotateItem = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (e.clientX > 1200 && e.clientY > 600) {
+      console.log('right bottom');
+      reps = 270;
+    } else if (e.clientX < 900 && e.clientY > 600) {
+      console.log('left bottom');
+      reps = 180;
+    } else if (e.clientX < 900 && e.clientY < 400) {
+      console.log('left top');
+      reps = 90;
+    } else if (e.clientX > 1200 && e.clientY < 400) {
+      console.log('right top');
+      reps = 0;
+    }
     const tl = gsap.timeline({ repeat: 0, repeatDelay: 0 });
-    tl.to('.items', { duration: 2, rotation: '+=90' }, 'spin');
-    tl.to('.item', { duration: 2, rotation: '-=90' }, 'spin');
+    tl.to('.items', { duration: 2, rotation: '+=' + reps }, 'spin');
+    tl.to('.item', { duration: 2, rotation: '-=' + reps }, 'spin');
   };
 
   return (
     <>
       <div className="items">
-        <div className="item item1" id="item1">
+        <div onClick={(e) => rotateItem(e)} className="item item1" id="item1">
           2
         </div>
-        <div className="item item2" id="item2">
+        <div onClick={(e) => rotateItem(e)} className="item item2" id="item2">
           1
         </div>
-        <div className="item item3" id="item3">
+        <div onClick={(e) => rotateItem(e)} className="item item3" id="item3">
           3
         </div>
-        <div className="item item4" id="item4">
+        <div onClick={(e) => rotateItem(e)} className="item item4" id="item4">
           4
         </div>
       </div>
-
-      <button onClick={() => rotateItem()}>PRESS ME</button>
     </>
   );
 };
